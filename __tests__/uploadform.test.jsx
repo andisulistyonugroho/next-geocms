b44bc5f883cfdb0948e5c79f8c9c8faf866dc26a
andisulistyonugroho <andisulistyonugroho@gmail.com>
2024-07-06 17:24:25 +0700

    test: init test for upload file test

---
+++ __tests__/uploadform.test.jsx
@@ -0,0 +1,11 @@
+describe("UploadForm", () => {
+  it("should have input file element", () => {});
+
+  it("should upload file happily", async () => {});
+
+  it("should show error message if upload non json file", async () => {});
+
+  it("should not show submit button if file type is invalid", async () => {});
+
+  it("should show submit button if file type is valid", () => {});
+});
