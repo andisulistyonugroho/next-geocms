5151adaac01e35fd0aae1116f8bd3174c0e1a6ba
andisulistyonugroho <andisulistyonugroho@gmail.com>
2024-07-07 06:38:04 +0700

    refactor: use dynamic render for map

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import dynamic from "next/dynamic";
 import MapVisual from "@/components/MapVisual";
 import UploadForm from "@/components/UploadForm";
+
+const DynamicMapVisual = dynamic(() => import("@/components/MapVisual"), {
+  loading: () => <p>A map is loading</p>,
+  ssr: false,
+});
 export default function Home() {
   return (
     <main>
       <div className="flex flex-wrap">
         <div className="w-full lg:w-3/4 py-4">
-          <MapVisual />
+          <DynamicMapVisual />
         </div>
         <div className="w-full font-mono p-4 lg:w-1/4">
           <UploadForm />
