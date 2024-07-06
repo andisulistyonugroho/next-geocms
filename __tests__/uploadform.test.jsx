dd73f18c3ceda97ccb97eadf0268b0e61bb9d0fd
andisulistyonugroho <andisulistyonugroho@gmail.com>
2024-07-07 05:23:39 +0700

    test: test condition for upload form

--- __tests__/uploadform.test.jsx
+++ __tests__/uploadform.test.jsx
@@ -1,11 +1,59 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import UploadForm from "@/components/UploadForm";
+
 describe("UploadForm", () => {
-  it("should have input file element", () => {});
+  it("should have input file element", () => {
+    render(<UploadForm />);
+    const input = screen.getByTestId("geojson-input");
+    expect(input).toBeInTheDocument();
+  });
 
-  it("should upload file happily", async () => {});
+  it("should upload file happily", async () => {
+    render(<UploadForm />);
+    const file2Upload = new File(["geo-dummy"], "geo.json", {
+      type: "application/json",
+    });
+    const input = screen.getByTestId("geojson-input");
+    await userEvent.upload(input, file2Upload);
+    expect(input.files.length).toBe(1);
+  });
 
-  it("should show error message if upload non json file", async () => {});
+  it("should show error message if upload non json file", async () => {
+    render(<UploadForm />);
+    const file2Upload = new File(["geo-dummy"], "geo.txt", {
+      type: "plain/text",
+    });
+    const input = screen.getByTestId("geojson-input");
+    await userEvent.upload(input, file2Upload);
+    expect(input.files.length).toBe(1);
+    const warningmessage = screen.getByTestId("warning-msg");
+    expect(warningmessage.textContent).toBe(
+      "invalid input, please uplaod json file"
+    );
+  });
 
-  it("should not show submit button if file type is invalid", async () => {});
+  it("should not show submit button if file type is invalid", async () => {
+    render(<UploadForm />);
+    const file2Upload = new File(["geo-dummy"], "geo.txt", {
+      type: "plain/text",
+    });
+    const input = screen.getByTestId("geojson-input");
+    await userEvent.upload(input, file2Upload);
+    expect(input.files.length).toBe(1);
+    expect(screen.queryByText("Submit")).toBeNull();
+  });
 
-  it("should show submit button if file type is valid", () => {});
+  it("should show submit button if file type is valid", async () => {
+    render(<UploadForm />);
+    const file2Upload = new File(["geo-dummy"], "geo.json", {
+      type: "application/json",
+    });
+    const input = screen.getByTestId("geojson-input");
+    await userEvent.upload(input, file2Upload);
+    expect(input.files.length).toBe(1);
+    const warningmessage = screen.getByTestId("warning-msg").toBeNull();
+    expect(screen.queryByText("Submit")).toBeInTheDocument();
+  });
 });
